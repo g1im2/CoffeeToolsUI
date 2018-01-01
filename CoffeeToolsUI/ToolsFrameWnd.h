0306f0d043d9515aee581e0f2bb895de61bb551c
Smith <smilsmile@gmail.com>
2018-01-01 22:17:15 +0800

    Ui change
    Ui布局，添加tab指向

--- CoffeeToolsUI/ToolsFrameWnd.h
+++ CoffeeToolsUI/ToolsFrameWnd.h
@@ -11,15 +11,17 @@ public:
 		CDialogBuilder builder;
 		CContainerUI* pFlushExamine = static_cast<CContainerUI*>(builder.Create(_T("tab_flush.xml"), (UINT)0));
 	
-		if (pFlushExamine) {
+		if (pFlushExamine) 
+		{
 			this->Add(pFlushExamine);
-		}
+		} 
 		
 		else {
 			this->RemoveAll();
 			return;
 		}
 	}
+
 };
 
 class CDialogBuilderCallbackEx : public IDialogBuilderCallback
@@ -28,6 +30,24 @@ public:
 	CControlUI* CreateControl(LPCTSTR pstrClass)
 	{
 		if (_tcscmp(pstrClass, _T("tab_flush")) == 0) return new ComputerExamineUI;
+		else if (_tcscmp(pstrClass, _T("tab_about")) == 0) 
+		{
+			CDialogBuilder builder;
+			CContainerUI* pAboutExamine = static_cast<CContainerUI*>(builder.Create(_T("tab_about.xml"), (UINT)0));
+			return pAboutExamine;
+		}
+		else if (_tcscmp(pstrClass, _T("tab_money")) == 0)
+		{
+			CDialogBuilder builder;
+			CContainerUI* pMoneyExamine = static_cast<CContainerUI*>(builder.Create(_T("tab_money.xml"), (UINT)0));
+			return pMoneyExamine;
+		}
+		else if (_tcscmp(pstrClass, _T("tab_tools")) == 0)
+		{
+			CDialogBuilder builder;
+			CContainerUI* pToolsExamine = static_cast<CContainerUI*>(builder.Create(_T("tab_tools.xml"), (UINT)0));
+			return pToolsExamine;
+		}
 
 		return NULL;
 	}
